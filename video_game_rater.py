62ac0ee7d2fa433c972a00ce50c27c4a9fb395fc
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-04-08 11:44:58 +1200

    Made main routine, initial collection
    Added a dictionary with video games and ratings

---
+++ video_game_rater.py
@@ -0,0 +1,10 @@
+##
+# video_game_rater.py
+# rates video games
+#
+# Author: Janelle Woolley
+# 08/04/22 Version_1
+
+if __name__ == "__main__":
+    video_games = {"Minecraft":5, "Call of Duty":1, "Angry Birds":4,
+                   "Splatoon 2":5, "Animal Crossing":4}
